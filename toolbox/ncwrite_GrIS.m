8e37fae9f96d6065bf34d9812c9256fece8037a9
hgoelzer <heiko@goelzers.de>
2019-02-03 20:52:17 +0100

    update mapping attributes and write from extra function ncwrite_mapping_GrIS.m

--- toolbox/ncwrite_GrIS.m
+++ toolbox/ncwrite_GrIS.m
@@ -64,13 +64,5 @@ ncwriteatt(ancfile,'y', 'axis', 'y') ;
 ncwriteatt(ancfile,'/','proj4','+init=epsg:3413')
 ncwriteatt(ancfile,'/','Description',['Created for ISMIP6 by Heiko Goelzer (h.goelzer@uu.nl), IMAU, ', date ])
 
-% Mapping information
-mapping = 'mapping';
-nccreate(ancfile,'mapping','Datatype','char');
-ncwriteatt(ancfile,'mapping', 'ellipsoid', 'WGS84') ;
-ncwriteatt(ancfile,'mapping', 'false_easting', '0.') ;
-ncwriteatt(ancfile,'mapping', 'false_northing', '0.') ;
-ncwriteatt(ancfile,'mapping', 'grid_mapping_name', 'polar_stereographic') ;
-ncwriteatt(ancfile,'mapping', 'latitude_of_projection_origin', '90.') ;
-ncwriteatt(ancfile,'mapping', 'standard_parallel', '70.') ;
-ncwriteatt(ancfile,'mapping', 'straight_vertical_longitude_from_pole', '-45') ;
+% Mapping
+ncwrite_mapping_GrIS(ancfile);
--- toolbox/ncwrite_GrIS_dSTdz.m
+++ toolbox/ncwrite_GrIS_dSTdz.m
@@ -1,6 +1,6 @@
-function ncwrite_GrIS(ancfile,avar,varname,dimnames,res,time)
+function ncwrite_GrIS(ancfile,avar,varname,dimnames,res,time,time_bounds)
 % write variables to a netcdf file and add ISMIP6 specific information
-% function ncwrite_GrIS(ancfile,avar,varname, dimnames, res, time)
+% function ncwrite_GrIS(ancfile,avar,varname, dimnames, res, time, time_bounds)
 
 % TODO: check input arguments and sizes
 
@@ -13,16 +13,18 @@ end
 dimnames{1}='x';
 dimnames{2}='y';
 dimnames{3}='time';
+dimnames{4}='nv';
 
 % Open netCDF file.
 ncid = netcdf.create(ancfile,'CLOBBER');
 
 % Define the dimensions of the variable.
 dimids=[];
-%for i=1:ndims(avar)
 for i=1:3
   dimids = [dimids,netcdf.defDim(ncid,dimnames{i},size(avar,i))];
 end
+netcdf.defDim(ncid,dimnames{4},2);
+
 % Define a new variable in the file.
 my_varID = netcdf.defVar(ncid,varname,'double',dimids);
 
@@ -54,38 +56,35 @@ end
 nccreate(ancfile,'x','Dimensions',{'x',nx}, 'Datatype','single', 'Format','classic');
 nccreate(ancfile,'y','Dimensions',{'y',ny}, 'Datatype','single', 'Format','classic');
 nccreate(ancfile,'time','Dimensions',{'time',1}, 'Datatype','single', 'Format','classic');
+nccreate(ancfile,'time_bounds','Dimensions',{'nv',2,'time',1}, 'Datatype','single', 'Format','classic');
 ncwrite(ancfile,'x',xd);
 ncwrite(ancfile,'y',yd);
-ncwrite(ancfile,'time',time-1900);
+ncwrite(ancfile,'time',time);
+ncwrite(ancfile,'time_bounds',time_bounds(:));
+
 ncwriteatt(ancfile,'x', 'units', 'm') ;
-ncwriteatt(ancfile,'y', 'units', 'm') ;
-ncwriteatt(ancfile,'time', 'units', 'seconds since 1900-07-01') ;
 ncwriteatt(ancfile,'x', 'standard_name', 'projection_x_coordinate') ;
-ncwriteatt(ancfile,'y', 'standard_name', 'projection_y_coordinate') ;
-ncwriteatt(ancfile,'time', 'standard_name', 'time axis') ;
 ncwriteatt(ancfile,'x', 'axis', 'x') ;
+ncwriteatt(ancfile,'y', 'units', 'm') ;
+ncwriteatt(ancfile,'y', 'standard_name', 'projection_y_coordinate') ;
 ncwriteatt(ancfile,'y', 'axis', 'y') ;
+ncwriteatt(ancfile,'time', 'units', 'days since 1900-1-1 00:00:00') ;
+ncwriteatt(ancfile,'time', 'standard_name', 'time') ;
+ncwriteatt(ancfile,'time', 'calendar', 'gregorian') ;
+ncwriteatt(ancfile,'time', 'bounds', 'time_bounds') ;
 ncwriteatt(ancfile,'time', 'axis', 'time') ;
 
 %% Attributes
 ncwriteatt(ancfile,'/','proj4','+init=epsg:3413')
-ncwriteatt(ancfile,'/','Description',['MAR ST change with surface elevation. Prepared for ISMIP6 by Heiko Goelzer (h.goelzer@uu.nl), IMAU, ', date ])
+ncwriteatt(ancfile,'/','Description',['MAR ST anomaly. Prepared for ISMIP6 by Heiko Goelzer (h.goelzer@uu.nl), IMAU, ', date ])
 ncwriteatt(ancfile,'/','MAR-contact','xavierfettweis@uliege.be')
 ncwriteatt(ancfile,'/','MAR-institute','University of Liege (Belgium)')
-
-% Mapping information
-mapping = 'mapping';
-nccreate(ancfile,'mapping','Datatype','char');
-ncwriteatt(ancfile,'mapping', 'ellipsoid', 'WGS84') ;
-ncwriteatt(ancfile,'mapping', 'false_easting', '0.') ;
-ncwriteatt(ancfile,'mapping', 'false_northing', '0.') ;
-ncwriteatt(ancfile,'mapping', 'grid_mapping_name', 'polar_stereographic') ;
-ncwriteatt(ancfile,'mapping', 'latitude_of_projection_origin', '90.') ;
-ncwriteatt(ancfile,'mapping', 'standard_parallel', '70.') ;
-ncwriteatt(ancfile,'mapping', 'straight_vertical_longitude_from_pole', '-45') ;
 
 % Add attributes to variables
 ncwriteatt(ancfile,varname, 'units', 'K m-1') ;
 ncwriteatt(ancfile,varname, 'long_name', 'Surface temperature change with surface elevation') ;
 ncwriteatt(ancfile,varname, '_FillValue', 9.96921e36) ; 
 ncwriteatt(ancfile,varname, 'grid_mapping', 'mapping') ;
+
+% Mapping
+ncwrite_mapping_GrIS(ancfile);
