8135524b44c4b5c9f5ef368c830aee09e2f4925d
hgoelzer <heiko@goelzers.de>
2019-02-13 07:40:16 +0100

    corrected typo on year

--- setup_data/process_MAR_reference.sh
+++ setup_data/process_MAR_reference.sh
@@ -10,8 +10,8 @@
 # MIROC5-rcp85_2006_2100/
 #	MARv3.9-yearly-MIROC5-rcp85-2006.nc
 
-datapath=/Volumes/ISMIP6/Data/Raw/SMB/MAR3.9/
-#datapath=/work/hgoelzer/Processing/RCM/MAR3.9/
+#datapath=/Volumes/ISMIP6/Data/Raw/SMB/MAR3.9/
+datapath=/work/hgoelzer/Processing/RCM/MAR3.9/
 
 gcm=MIROC5
 scen=rcp85
@@ -37,7 +37,7 @@ done
 ncrcat -O MAR_1960.nc MAR_1961.nc MAR_1962.nc MAR_1963.nc MAR_1964.nc MAR_1965.nc MAR_1966.nc MAR_1967.nc MAR_1968.nc MAR_1969.nc MAR_1970.nc MAR_1971.nc MAR_1972.nc MAR_1973.nc MAR_1974.nc MAR_1975.nc MAR_1976.nc MAR_1977.nc MAR_1978.nc MAR_1979.nc MAR_1980.nc MAR_1981.nc MAR_1982.nc MAR_1983.nc MAR_1984.nc MAR_1985.nc MAR_1986.nc MAR_1987.nc MAR_1988.nc MAR_1989.nc  MARv3.9-yearly-${gcm}-${scen}-1960-1989.nc
 
 # Long-term average
-ncra -O  MARv3.9-yearly-${gcm}-${scen}-1960-1989.nc MARv3.9-yearly-${gcm}-${scen}-ltm1960-2089.nc
+ncra -O  MARv3.9-yearly-${gcm}-${scen}-1960-1989.nc MARv3.9-yearly-${gcm}-${scen}-ltm1960-1989.nc
 
 # Copy to destination
 /bin/cp MARv3.9-yearly-${gcm}-${scen}-ltm1960-1989.nc ../../Data/MAR/
