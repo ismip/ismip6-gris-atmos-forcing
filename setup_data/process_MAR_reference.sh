9db47e5eb44e5318afe6bdd32d443ac5d1256ef3
hgoelzer <heiko@goelzers.de>
2019-03-29 14:37:32 +0100

    Corrected typo

--- setup_data/process_MAR_reference.sh
+++ setup_data/process_MAR_reference.sh
@@ -30,7 +30,7 @@ done
 ncrcat -O MAR_1960.nc MAR_1961.nc MAR_1962.nc MAR_1963.nc MAR_1964.nc MAR_1965.nc MAR_1966.nc MAR_1967.nc MAR_1968.nc MAR_1969.nc MAR_1970.nc MAR_1971.nc MAR_1972.nc MAR_1973.nc MAR_1974.nc MAR_1975.nc MAR_1976.nc MAR_1977.nc MAR_1978.nc MAR_1979.nc MAR_1980.nc MAR_1981.nc MAR_1982.nc MAR_1983.nc MAR_1984.nc MAR_1985.nc MAR_1986.nc MAR_1987.nc MAR_1988.nc MAR_1989.nc  MARv3.9-yearly-${gcm}-1960-1989.nc
 
 # Long-term average
-ncra -O  MARv3.9-yearly-${gcm-1960-1989.nc MARv3.9-yearly-${gcm}-ltm1960-1989.nc
+ncra -O  MARv3.9-yearly-${gcm}-1960-1989.nc MARv3.9-yearly-${gcm}-ltm1960-1989.nc
 
 # Copy to destination
 /bin/cp MARv3.9-yearly-${gcm}-ltm1960-1989.nc ../../Data/MAR/
